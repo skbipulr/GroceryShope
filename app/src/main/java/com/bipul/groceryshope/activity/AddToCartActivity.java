0e38f71e17b07d26a144b97127e28cc67d6a04a5
Bipul Sarkar <skbipulr@gmail.com>
2020-05-15 13:04:52 +0600

    add checkout design feature

---
+++ app/src/main/java/com/bipul/groceryshope/activity/AddToCartActivity.java
@@ -0,0 +1,16 @@
+package com.bipul.groceryshope.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bipul.groceryshope.R;
+
+public class AddToCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_cart);
+    }
+}
